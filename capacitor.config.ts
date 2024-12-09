d2839218004a7be8f0989249f76fe193668007fb
turtureanu <90131332+turtureanu@users.noreply.github.com>
2024-12-09 19:53:14 +0100

    add(deps): Svelte, Capacitor, Skeleton UI

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'org.maturitaassistant.app',
+	appName: 'Maturita Assistant',
+	webDir: 'dist'
+};
+
+export default config;
